8b2b8140fa1a0228d1ffd13d6bb648ad70494127
EmissaryOfVddd <vdddenjoyer@gmail.com>
2024-03-20 22:49:06 +1000

    Finished first chapter

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
+use crate::cpu::{constants, CPU};
+
+mod cpu;
+
 fn main() {
-    println!("Hello, world!");
+    let mut cpu = CPU::new();
+    cpu.interpret(vec![0xA9, 0x05, 0x00]);
+    assert_eq!(cpu.register_a, 0x05);
+    assert!(cpu.status & constants::ZERO_FLAG == 0b00);
+    assert!(cpu.status & constants::NEGATIVE_FLAG == 0b00);
 }
